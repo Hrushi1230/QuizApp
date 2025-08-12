bb373d1c9e9dc03d677f97af6393de33bf0daa03
Hrushikesh <hrushisahu973@gmail.com>
2025-08-12 12:46:55 +0530

    i have done basic html , css and gets some questions data in script file

---
+++ scripts.js
@@ -0,0 +1,29 @@
+
+const questions =[
+    {
+        question : "What is the capital of India?",
+        options : ["Mumbai" , "New Delhi" , "Kolkta" ,"Bhubaneswar"],
+        answer : "New Delhi"
+    },
+    {
+        question : "What is 19 +69 =_ ?" ,
+        options : ["78" , "87" , "88" , "98"],
+        answer : "88"
+    },
+    {
+        question : "What is the capital of Odisha?",
+        options : ["Bhubaneswar" , "Kolkata" , "Mumbai" ,"New Delhi"],
+        answer : "Bhubaneswar"
+    },
+    {
+        question : "what is 21 -9 =_ ?",
+        options : ["12" , "13" , "14" , "15"],
+        answer : "12" 
+    },
+    {
+        question : "Who is the Prime Minister of India?",
+        options : ["Narendra Modi" , "Rahul Gandhi" , "Amit Shah" ,"Arvind Kejriwal"],
+        answer : "Narendra Modi"
+    }
+];
+
