fe3aa1951ae24f0aadecffe70f512cfbd43808f2
Hrushikesh <hrushisahu973@gmail.com>
2025-08-12 16:25:00 +0530

    i am try to setup skeleton and dom reference

--- scripts.js
+++ scripts.js
@@ -27,3 +27,33 @@ const questions =[
     }
 ];
 
+//Dom References
+ const questionContainer =document.getElementById("question-container");
+ const optionsContainer = document.getElementById("option-container");
+ const timerDisplay =document.getElementById("timer");
+ const nextBtn =document.getElementById("next-btn");
+ const resultDiv =document.getElementById("result");
+
+ //State Variables
+
+  let currentQuestionIndex = 0;
+  let score =0;
+  let hasAnswered =false;
+
+  //Start the function
+
+  function startQuiz() {
+
+    currentQuestionIndex =0 ;
+    score =0 ;
+    resultDiv.classList.add('hide');
+    nextBtn.style.display = "none";
+    questionContainer.innerText = "Ready - press Next to start";
+    optionsContainer.innerHTML = "";
+    timerDisplay.innerText = "Time Left :_";
+
+    nextBtn.addEventListener("click" , () =>{
+         console.log("Next button clicked");
+    });
+  }
+  startQuiz();
